58a7a64cf46296086c12b022bbefcab740f5b55a
Jack Branch <jackthomasbranch@yahoo.com>
2018-06-25 18:45:52 +0100

    more changes

--- directory.rb
+++ directory.rb
@@ -4,28 +4,59 @@ def print_header
 end
 
 def print_names(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]}) cohort"
-  end
+  # i = 0
+  # until i == students.length
+  #   puts "#{i+1}: #{students[i][:name]} (#{students[i][:cohort]}) cohort"
+  #   i += 1
+  # end
+  # by cohort
+  students.sort_by! { |k, v| k[:cohort]}
+  students.each_with_index do |student, i|
+     puts "#{i+1}: #{student[:name]} (#{student[:cohort]}) cohort"
+   end
+  
+end
+
+def first_letter(students, l)
+
+  students.each { |student| if student[:name][0] == l
+                           puts "#{student[:name]}" end }
+end
+
+def max_length(students, l=12)
+  students.each { |student| puts student[:name].length < 12 ? student[:name]
+                                                            : ""}
 end
 
 def print_footer(num)
-  print "unfortunately there are #{num} students"
+  print "unfortunately there "
+  print num > 1 ? "are #{num} students" : "is #{num} student"
 end
 
-students = [
-  { name: "Ginger Chav", cohort: :november },
-  { name: "Chav Chavington", cohort: :december },
-  { name: "Ali G", cohort: :february },
-  { name: "Vicky Pollard", cohort: :june },
-  { name: "Jeremy Corbyn", cohort: :december },
-  { name: "All Fortnite Players", cohort: :may },
-  { name: "H-Annibalzz", cohort: :september },
-  { name: "Dr Lectoz", cohort: :july },
-  {name: "Dex Lex AzluthorZizzle", cohort: :september }
-]
-student_count = students.length
+def input_students
+  puts "enter the names of the students".center(12),
+        "to finish enter return twice".center(8)
+  students = []
+  name = gets.chomp # or name.strip! if 1st and last chars whitespace :
+                    # if name[1] && name[name.length -1] == " " then name.strip!
+  while !name.empty? do
+    puts "enter a cohort please"
+    cohor = gets.chomp
+    students << { name: name, cohort: cohor }
+    puts "now we have #{students.count} students"
+    name = gets.chomp
+  end
+  return students
+end
+
 
 print_header
-print_names(students)
-print_footer(student_count)
+students = input_students
+
+student_count = students.length
+if students.length > 0  # test for no additions by user
+  print_names(students)
+  print_footer(student_count)
+  #first_letter(students, "j")
+  #max_length(students)
+else puts "error no students added" end
