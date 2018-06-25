3c353d8c245da823e4f8bb26ccd09e6f1baf245d
Jack Branch <jackthomasbranch@yahoo.com>
2018-06-25 23:44:14 +0100

    moer changz

--- directory.rb
+++ directory.rb
@@ -10,11 +10,11 @@ def print_names(students)
   #   i += 1
   # end
   # by cohort
-  students.sort_by! { |k, v| k[:cohort]}
+  #students.sort_by! { |k, v| k[:cohort]}
   students.each_with_index do |student, i|
      puts "#{i+1}: #{student[:name]} (#{student[:cohort]}) cohort"
    end
-  
+
 end
 
 def first_letter(students, l)
@@ -49,14 +49,86 @@ def input_students
   return students
 end
 
+def show_students(students)
+  print_header
+  print_names(students)
+  print_footer(students.length)
+end
 
-print_header
-students = input_students
+def menu_select(selection, students)
+  case selection
+    when "1"
+      students += input_students
+    when "2"
+      puts students
+      show_students(students)
+    when "3"
+      write_file(students, "students.csv")
+    when "4"
 
+    when "5"
+
+    when "6"
+
+    when "7"
+
+    when "8"
+
+    when "9"
+      exit
+    else
+      puts "oi oi batty boy put in a number siv vouz plaiz"
+    end
+
+  return students
+end
+
+def read_file(filename)
+  output = []
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    output << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+  return output
+
+end
+
+def write_file(text, filename)
+  file = File.open(filename, "w")
+  puts text
+  text.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def interactive_menu
+  students = read_file("students.csv")
+  loop do
+
+    puts "\n1. input students\n2. show students\n3. save to file\n4. \n5." +
+         "\n6. \n7. \n8. \n9. Exit"
+    students = menu_select(gets.chomp, students)
+
+
+  end
+
+
+end
+
+interactive_menu
 student_count = students.length
+
+
+=begin
 if students.length > 0  # test for no additions by user
   print_names(students)
   print_footer(student_count)
   #first_letter(students, "j")
   #max_length(students)
 else puts "error no students added" end
+=end
